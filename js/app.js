28f4c0148171660c693ce0dd1bc18d94dbcff312
Mustafa Ngure Khamisi <mustafangure@gmail.com>
2018-08-18 16:33:40 +0300

    feat: Access DOM elements and add interactivity to the page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,39 @@
 /*
  * Create a list that holds all of your cards
  */
+const stars = document.querySelector('.stars');
+const allCards = document.querySelectorAll('.card');
+const cardIcons= document.querySelectorAll('.card i');
+const moves = document.querySelector('.moves');
+const restart = document.querySelector('.restart');
+const overlay = document.querySelector('.overlay');
+const score = document.querySelector('#score_message');
+const timer = document.querySelector('.timer');
+let min = document.querySelector('#minutes');
+let sec = document.querySelector('#seconds');
+const play = document.querySelector('#play');
 
+// Array to hold all the classes of the icons.
+let allClasses = [];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+function storeIcons() {
+    for(let value of cardIcons) {
+        allClasses.push(value.className);
+    }
+}
+
+// reshuffle all the icons on the cards
+let shuffledIcons = [];
+function reShuffle(){
+    allClasses = [];
+    shuffledIcons = [];
+    storeIcons();
+    shuffledIcons = shuffle(allClasses);
+    cardIcons.forEach(function(icon,i,arr) {
+        arr[i].classList.value = shuffledIcons.pop();
+    });
+}
+reShuffle();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,14 +50,177 @@ function shuffle(array) {
     return array;
 }
 
+// Remove modal and restart the game.
+function hideModal() {
+    overlay.classList.add('hidden');
+    startGame();
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// display modal if all cards have been matched.
+function showModal() {
+    const documentFrag = document.createDocumentFragment();
+    // You can't set the innerHTML of a document fragment like you would do with a normal node, therefore
+    // adding a standard div and setting the innerHTML of that is the common solution.
+    const modalContainer = document.createElement('div');
+    overlay.classList.remove('hidden');
+    scoreMsg();
+    
+    modalContainer.innerHTML = `<div id="modal">
+    <div id="score_message">${msg}</div>
+        <table id="modalTable">
+            <tbody>
+                <tr>
+                    <td>Rating :</td>
+                    <td class="stars_list">${stars.innerHTML}</td>
+                </tr>
+                <tr>
+                    <td>Time :</td>
+                    <td>${timer.textContent}</td>
+                </tr>
+                <tr>
+                    <td>Moves :</td>
+                    <td>${count}</td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="button" id="play" onclick="hideModal();" value="Play Again"/></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>`;
+    documentFrag.appendChild(modalContainer);
+    overlay.appendChild(documentFrag);
+}
+
+// message to display on the modal depending on the numbers of moves.
+let msg = '';
+function scoreMsg() {
+    if (count < 2) {
+        msg = "Congratulations";
+    } else if(count > 2 && count <= 5){
+        msg = "Nice try";
+    }else{
+        msg = "You can do better!";
+    }
+    return msg;
+}
+// if all the cards are matched, show the modal.
+function endGame() {
+    if(matchedCards.length === 16){
+        clearInterval(runTimer);
+        showModal();
+    }
+}
+
+let count = 0;
+let seconds = 0;
+
+// display time as two digits. start with zero for numbers less than or equal to 9.
+function checkTime(value) {
+    return value <= 9 ? '0' + value : value;
+}
+
+//increment the seconds and display the time on the DOM element.
+function configTime(){
+    ++seconds;
+    sec.textContent = checkTime(seconds%60);
+    min.textContent = checkTime(parseInt(seconds/60,10));
+}
+
+// start timer
+var runTimer = setInterval(configTime,1000);
+
+//Decrease the star rating after some number of moves.
+const stars_list = stars.children;
+function rating(){
+    if(count === 10){
+        stars_list[2].setAttribute("style", "visibility: hidden");
+    }else{
+        if(count === 16){
+            stars_list[1].setAttribute("style", "visibility: hidden");
+        }
+    }
+    
+}
+
+// count number of moves and display on the DOM element.
+function counter(){
+    ++count;
+    moves.textContent = `${count}`;
+    rating();
+}
+
+// Array that holds all matched cards.
+let matchedCards = [];
+
+// confirm if the opened cards match.
+function checkMatch(event) {
+    if(openedCards.length === 2){
+        counter();
+        if(openedCards[0].firstElementChild.className === openedCards[1].firstElementChild.className){
+            for(let value of openedCards){
+                value.classList.add('match');
+                matchedCards.push(value);
+            }
+            openedCards = [];
+            endGame();
+        }else{
+            for(let value of openedCards){
+                value.classList.add('fail');
+            }
+            setTimeout(function(){
+                for(let value of openedCards){
+                    value.classList.remove('show','fail','open');
+                }
+                openedCards = [];
+            }, 800);            
+        }
+    }
+}
+
+//open the clicked card and put it in a temporary array that holds opened cards.
+function openCard(event) {
+    const clickedCard = event.target;
+    //check if the click is the right target (a LI element).
+    if(clickedCard.nodeName === 'LI') {
+        openedCards.push(clickedCard);
+    }
+    checkMatch(event);
+}
+
+//open and show clicked card if it is not yet open.
+function flipCard(evt) {
+    if(!evt.target.classList.contains('open')){
+        evt.target.classList.add('show','open');
+        openCard(evt);
+    }
+}
+
+//A temporary array to hold opened cards.
+let openedCards = [];
+allCards.forEach(function (value) { 
+    value.addEventListener('click',flipCard);
+});
+
+//reset
+function startGame (){
+    for(let star of stars_list){
+        star.removeAttribute("style", "visibility: hidden");
+    }
+    count = 0;
+    seconds = 0;
+    clearInterval(runTimer);
+    sec.textContent = '';
+    min.textContent = '';
+    sec.textContent = `0${Number('0')}`;
+    min.textContent = `0${Number('0')}`;
+    setInterval(configTime,1000);
+    moves.textContent = `${count}`;
+    allCards.forEach(function (value) {
+        value.classList.remove('open','show','match');
+    });
+    openedCards = [];
+    matchedCards = [];
+    overlay.innerHTML = ' ';
+    reShuffle();
+}
+restart.addEventListener('click',startGame);
